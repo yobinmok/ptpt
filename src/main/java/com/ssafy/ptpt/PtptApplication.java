2b205ec275b7389788123059cf222125e1f4a3a7
강지우 <jiu6525@daum.net>
2024-07-22 17:24:52 +0900

    Initial commit

---
+++ src/main/java/com/ssafy/ptpt/PtptApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ptpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/ptpt/PtptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ptpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
