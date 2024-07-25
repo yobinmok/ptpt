f79fc2787dd40ed957ecf4f2431e60ecbcfde4f6
kkkhhh4930 <kkkhhh4930@naver.com>
2024-07-25 12:57:51 +0900

    feat: add main, login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage'
 import SignupPage from './pages/SignupPage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
