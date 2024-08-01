9758996f896d6324d22f4bbbb6ca0dfedf432e50
강지우 <jiu6525@daum.net>
2024-08-01 23:24:27 +0900

    refactor : 변수 수정

--- backend/src/main/java/com/ssafy/ptpt/api/studyroom/controller/StudyRoomController.java
+++ backend/src/main/java/com/ssafy/ptpt/api/studyroom/controller/StudyRoomController.java
@@ -54,9 +54,9 @@ public class StudyRoomController {
             @ApiResponse(responseCode = "200", description = "Success"),
             @ApiResponse(responseCode = "404", description = "Not Found"),
     })
-    @DeleteMapping("/{memberId}")
+    @DeleteMapping("/{studyRoomId}")
     @Operation(summary = "스터디룸 삭제")
-    public ResponseEntity<Void> deleteRoom(@PathVariable("memberId") @RequestBody Long memberId) {
+    public ResponseEntity<Void> deleteRoom(@PathVariable("studyRoomId") @RequestBody Long memberId) {
         studyRoomService.deleteStudyRoom(memberId);
         return ResponseEntity.ok().build();
     }
