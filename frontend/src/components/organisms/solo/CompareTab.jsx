b6579167479a6a6f92d3bf3486411eb3b530baa4
목요빈 <dyqls1214@naver.com>
2024-08-02 13:51:41 +0900

    feat: 비교 탭 레이아웃

--- frontend/src/components/organisms/solo/CompareTab.jsx
+++ frontend/src/components/organisms/solo/CompareTab.jsx
@@ -1,5 +1,45 @@
+import React, { useState } from 'react';
+import { Button, Box, Divider } from '@mui/material';
+import CustomSelect from '../../molecules/CustomSelect';
+import CustomSlider from '../../molecules/CustomSlider';
+import { useSelector, useDispatch } from 'react-redux';
+import { registerGuideline } from '../../../store/actions/solo';
 const CompareTab = () => {
-  return <>비교 탭 content</>;
+  const dispatch = useDispatch();
+  let soloPreset = useSelector((state) => state.solo);
+  let scriptIdx = 0;
+
+  return (
+    <>
+      <CustomSelect
+        label='가이드라인 선택'
+        options={soloPreset.guideline.map((item, index) => ({
+          value: index,
+          label: item,
+        }))}
+        onChange={(value) => {}}
+      />
+      <CustomSelect
+        label='녹음본 선택'
+        options={soloPreset.voiceRecord.map((item, index) => ({
+          value: index,
+          label: item,
+        }))}
+        onChange={(value) => {}}
+        style={{ marginTop: '10px' }}
+      />
+
+      <Button
+        onClick={() => {
+          dispatch(registerGuideline(scriptIdx, voiceSetting));
+        }}
+        variant='contained'
+        color='secondary'
+      >
+        등록
+      </Button>
+    </>
+  );
 };
 
 export default CompareTab;
