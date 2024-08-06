fcf9f14c67a561c86a39b2a94e757868f61c6921
임권 <limkwon18@gmail.com>
2024-08-06 09:21:59 +0900

    fix : make url use as variable

--- backend/src/main/java/com/ssafy/ptpt/config/CorsConfig.java
+++ backend/src/main/java/com/ssafy/ptpt/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.ptpt.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,13 +11,15 @@ import java.util.List;
 
 @Configuration
 public class CorsConfig {
+    @Value("${react.server.address}")
+    private String REACT_SERVER_URL;
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(REACT_SERVER_URL));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
