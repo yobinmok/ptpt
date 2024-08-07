aa4d5962716fa25650540b0547e207b90b6c3ccc
강지우 <jiu6525@daum.net>
2024-08-07 15:26:15 +0900

    refactor : 평가 쿼리수정

--- backend/src/main/java/com/ssafy/ptpt/db/jpa/repository/EntryListRepository.java
+++ backend/src/main/java/com/ssafy/ptpt/db/jpa/repository/EntryListRepository.java
@@ -2,8 +2,12 @@ package com.ssafy.ptpt.db.jpa.repository;
 
 import com.ssafy.ptpt.db.jpa.entity.EntryList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EntryListRepository extends JpaRepository<EntryList, Long> {
     void deleteByStudyRoomId(Long studyRoomId);
-    Long findStudyRoomIdByMemberId(Long MemberId);
+
+    @Query("SELECT e.studyRoomId FROM EntryList e WHERE e.memberId = :memberId")
+    Long findStudyRoomIdByMemberId(@Param("memberId") Long memberId);
 }
