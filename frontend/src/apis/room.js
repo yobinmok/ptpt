54638fda1341319c0f51d2c7e94d1bcbb359c3b0
황용주 <ek6015@naver.com>
2024-08-08 15:18:58 +0900

    refact : 평가 탭 수정

--- frontend/src/apis/room.js
+++ frontend/src/apis/room.js
@@ -106,9 +106,9 @@ export const takeMyEvaluate = async (studyRoomId, userId) => {
   try {
     const response = await axios.post(`/evaluation/feedBack`, {
       studyRoomId: studyRoomId,
-      oauthId: 'G123',
+      oauthId: userId,
     });
-    return response;
+    return response.data;
   } catch (error) {
     console.log('take my evaluate info error : ' + error);
   }
