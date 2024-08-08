c68c763c5f2763347c51a1440190bb08442dd651
목요빈 <dyqls1214@naver.com>
2024-08-08 14:50:03 +0900

    feat: 음성모델 등록화면 ing

--- frontend/src/components/organisms/myInfo/VoiceModelPage.jsx
+++ frontend/src/components/organisms/myInfo/VoiceModelPage.jsx
@@ -1,22 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  fetchVoiceModel,
-  uploadVoiceModel,
-} from '../../../store/actions/voiceModelActions';
+import { uploadVoiceModel } from '../../../store/actions/voiceModelActions';
+import { getProfile } from '../../../apis/auth';
 
 const VoiceModelPage = () => {
   const dispatch = useDispatch();
-  const voiceModel = useSelector((state) => state.voiceModel.data);
-  const error = useSelector((state) => state.voiceModel.error);
-  const oauthId = useSelector((state) => state.user.data?.id); // 사용자 ID 가져오기
+  // const voiceModel = useSelector((state) => state.voiceModel.data);
+  // const error = useSelector((state) => state.voiceModel.error);
+  const oauthId = useSelector((state) => state.auth.user.oauthId);
   const [file, setFile] = useState(null);
+  const [voiceModelCreated, setVoiceModelCreated] = useState(false);
 
-  useEffect(() => {
-    if (oauthId) {
-      dispatch(fetchVoiceModel(oauthId));
-    }
-  }, [dispatch, oauthId]);
+  useEffect(async () => {
+    const profile = await getProfile(oauthId);
+    console.log(profile);
+    setVoiceModelCreated(profile.voiceModelCreated);
+  }, []);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -35,11 +34,8 @@ const VoiceModelPage = () => {
   return (
     <div>
       <h1>음성 모델 페이지</h1>
-      {voiceModel ? (
-        <div>
-          <p>Voice Model ID: {voiceModel.id}</p>
-          <p>Voice Model Description: {voiceModel.description}</p>
-        </div>
+      {voiceModelCreated ? (
+        <div>음성 모델이 등록되어 있습니다.</div>
       ) : (
         <p>No voice model available.</p>
       )}
