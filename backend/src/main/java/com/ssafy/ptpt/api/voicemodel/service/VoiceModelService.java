f7cd54e927387eab2952b144a3ef1f556b946ed5
목요빈 <dyqls1214@naver.com>
2024-08-11 11:33:50 +0900

    feat: rvc 업로드 전체 수정사항

--- backend/src/main/java/com/ssafy/ptpt/api/voicemodel/service/VoiceModelService.java
+++ backend/src/main/java/com/ssafy/ptpt/api/voicemodel/service/VoiceModelService.java
@@ -1,5 +1,6 @@
 package com.ssafy.ptpt.api.voicemodel.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -30,6 +31,8 @@ public class VoiceModelService {
     private String CONVERT; // 추후 application.yml 파일에 추가 예정
     @Value("${external.api.select}")
     private String SELECT;
+    @Value("${external.api.upload}")
+    private String UPLOAD;
 
     @Autowired
     public VoiceModelService(WebClient.Builder webClientBuilder, ObjectMapper objectMapper, MemberRepository memberRepository) {
@@ -38,15 +41,55 @@ public class VoiceModelService {
         this.memberRepository = memberRepository;
     }
 
+    public Mono<String> uploadAudioFile(String ttsPath) {
+        System.out.println("in uploadAudioFile");
+//        {
+//            "data": [
+//                "http://70.12.130.121:7897/file=/tmp/gradio/a8ef0ece1af5d8a3b50d911703d5a670d23d1834/audio.wav",
+//                true
+//            ]
+//        }
+        String resultPath = "https://i11b207.p.ssafy.io" + ttsPath.substring(ttsPath.indexOf("/uploads"));
+
+        // JSON 객체 생성
+        ObjectNode jsonObject = objectMapper.createObjectNode();
+        ArrayNode jsonArray = objectMapper.createArrayNode();
+        jsonArray.add(resultPath);
+        jsonArray.add(false); // convert 시에는 false
+        jsonObject.set("data", jsonArray);
+
+        return webClient.post()
+                .uri(UPLOAD)
+                .bodyValue(jsonObject)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> inferChangeVoice(String vmName) {
+        System.out.println("in inferChangeVoice");
+
+        // JSON 객체 생성
+        ObjectNode jsonObject = objectMapper.createObjectNode();
+        ArrayNode jsonArray = objectMapper.createArrayNode();
+        jsonArray.add(vmName);
+        jsonArray.add(0.33);
+        jsonArray.add(0.33);
+        jsonObject.set("data", jsonArray);
+
+        return webClient.post()
+                .uri(SELECT)
+                .bodyValue(jsonObject)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
     public Mono<String> inferConvert(String ttsPath) {
         System.out.println("in inferConvert");
-        String resultPath = "https://i11b207.p.ssafy.io" + ttsPath.substring(ttsPath.indexOf("/uploads"));
-        System.out.println(resultPath);
         // JSON 객체 생성
         ObjectNode jsonObject = objectMapper.createObjectNode();
         ArrayNode jsonArray = objectMapper.createArrayNode();
         jsonArray.add(0);
-        jsonArray.add(resultPath); // "C:\\Users\\SSAFY\\Desktop\\src\\tts.wav"
+        jsonArray.add(ttsPath); // "C:\\Users\\SSAFY\\Desktop\\src\\tts.wav"
         jsonArray.add(0);
         jsonArray.addNull();
         jsonArray.add("rmvpe");
@@ -66,39 +109,35 @@ public class VoiceModelService {
                 .bodyToMono(String.class);
     }
 
-    public Mono<String> inferChangeVoice(String fileName) {
-        System.out.println("in inferChangeVoice");
-
-        // JSON 객체 생성
-        ObjectNode jsonObject = objectMapper.createObjectNode();
-        ArrayNode jsonArray = objectMapper.createArrayNode();
-        jsonArray.add(fileName);
-        jsonArray.add(0.33);
-        jsonArray.add(0.33);
-        jsonObject.set("data", jsonArray);
-
-        return webClient.post()
-                .uri(SELECT)
-                .bodyValue(jsonObject)
-                .retrieve()
-                .bodyToMono(String.class);
-    }
-
     // 음성 변환 프로세스를 체이닝하는 메서드
-    public Mono<String> processVoiceConversion(String fileName, String ttsPath) {
-        return inferChangeVoice(fileName)
-                .flatMap(response -> inferConvert(ttsPath))
-                .flatMap(response -> {
-                    ObjectMapper objectMapper = new ObjectMapper();
+    public Mono<String> processVoiceConversion(String voiceModel, String ttsPath) {
+        return uploadAudioFile(ttsPath)
+                .flatMap(uploadResponse -> {
+                    // 여기서 `uploadResponse`는 `uploadAudioFile`의 응답입니다.
+                    // inferChangeVoice 호출 후, 이 응답을 inferConvert에 사용합니다.
+                    String uploadPath;
                     try {
-                        JsonNode rootNode = objectMapper.readTree(response); // JSON 문자열을 JsonNode로 변환
-                        JsonNode dataNode = rootNode.path("data").get(1); // data 배열의 두 번째 요소 가져오기
-                        String filePath = dataNode.path("name").asText(); // name 값 추출
-                        System.out.println("data[1].name: " + filePath);
-                        return Mono.just(convertFileToBase64(filePath));
-                    } catch (Exception e) {
-                        return Mono.error(e);
+                        JsonNode rootNode = objectMapper.readTree(uploadResponse);
+                        uploadPath = rootNode.path("data").get(0).asText();
+                    } catch (JsonProcessingException e) {
+                        return Mono.error(new RuntimeException(e));
                     }
+                    System.out.println("upload 경로 확인: " + uploadPath);
+                    return inferChangeVoice(voiceModel)
+                            .flatMap(changeVoiceResponse -> inferConvert(uploadPath))
+                            .flatMap(convertResponse -> {
+                                ObjectMapper objectMapper = new ObjectMapper();
+                                try {
+                                    JsonNode rootNode = objectMapper.readTree(convertResponse); // JSON 문자열을 JsonNode로 변환
+                                    JsonNode dataNode = rootNode.path("data").get(1); // data 배열의 두 번째 요소 가져오기
+                                    String filePath = dataNode.path("name").asText(); // name 값 추출
+                                    System.out.println("data[1].name: " + filePath);
+                                    String resultUrl  ="http://70.12.130.121/:7897/file=" + filePath;
+                                    return Mono.just(resultUrl);
+                                } catch (Exception e) {
+                                    return Mono.error(e);
+                                }
+                            });
                 })
                 .onErrorResume(error -> {
                     System.err.println("오류 발생: " + error.getMessage());
