e8b17ac9200e959d98791da8967f9ecd54f4d665
황용주 <ek6015@naver.com>
2024-08-12 17:11:58 +0900

    refat : participant redux

--- frontend/src/store/types/participant.js
+++ frontend/src/store/types/participant.js
@@ -5,3 +5,4 @@ export const USE_PARTICIPANTS = 'USE_PARTICIPANTS'; // 탭을 통해 참가자 �
 
 // 평가확인 목록
 export const EVAL_PARTICIPANTS = 'EVAL_PARTICIPANTS';
+export const CLEAR_PARTICIPANTS = 'CLEAR_PARTICIPANTS';
