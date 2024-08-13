1f45b2d78b0b4ed0b332a1f29310443b775932a3
강지우 <jiu6525@daum.net>
2024-08-13 09:57:57 +0900

    f

--- backend/src/main/java/com/ssafy/ptpt/oauth2/hadler/CustomSuccessHandler.java
+++ backend/src/main/java/com/ssafy/ptpt/oauth2/hadler/CustomSuccessHandler.java
@@ -1,11 +1,14 @@
 package com.ssafy.ptpt.oauth2.hadler;
 
+import com.ssafy.ptpt.db.jpa.entity.Member;
+import com.ssafy.ptpt.db.jpa.repository.MemberRepository;
 import com.ssafy.ptpt.jwt.JWTUtil;
 import com.ssafy.ptpt.oauth2.dto.CustomOAuth2User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -17,17 +20,20 @@ import java.util.Collection;
 import java.util.Iterator;
 
 @Component
+@RequiredArgsConstructor
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     @Value("${react.server.address}")
     private String REACT_SERVER;
 
     private final JWTUtil jwtUtil;
+    private final MemberRepository memberRepository;
 
-    public CustomSuccessHandler(JWTUtil jwtUtil) {
-
-        this.jwtUtil = jwtUtil;
-    }
+//    public CustomSuccessHandler(JWTUtil jwtUtil, MemberRepository memberRepository) {
+//
+//        this.jwtUtil = jwtUtil;
+//        this.memberRepository = memberRepository;
+//    }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -42,7 +48,6 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         GrantedAuthority auth = iterator.next();
         String role = auth.getAuthority();
 
-
         // oauthId 를 얻어오는 로직
         // 문자열을 공백으로 분리
         String[] parts = username.split(" ");
@@ -54,10 +59,18 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         System.out.println("oauthId: " + oauthId);
 
         System.out.println("username: " + username + " role: " + role);
-        String token = jwtUtil.createJwt(username, role, oauthId, 60*60*60L);
+        String token = jwtUtil.createJwt(username, role, oauthId, 10 * 60 * 60 * 1000L);
 
         response.addCookie(createCookie("Authorization", token));
-        response.sendRedirect(REACT_SERVER + "userinfo");
+
+        Member member = memberRepository.findByOauthId(oauthId);
+        if(member.getNickname() == null){
+            System.out.println(" 닉네임 설정 페이지로 이동 ");
+            response.sendRedirect(REACT_SERVER + "userinfo?oauthId=" + oauthId);
+        }else{
+            response.sendRedirect(REACT_SERVER + "?oauthId=" + oauthId);
+        }
+
     }
 
     private Cookie createCookie(String key, String value) {
@@ -66,8 +79,8 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         cookie.setMaxAge(60*60*60);
         //cookie.setSecure(true);
         cookie.setPath("/");
-        
-        cookie.setHttpOnly(true);
+
+//        cookie.setHttpOnly(true);
 
         return cookie;
     }
