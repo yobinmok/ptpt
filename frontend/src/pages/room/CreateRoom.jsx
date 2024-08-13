3f1bc6a13424557cef9dda2fe506c2a18bb3ad6c
목요빈 <dyqls1214@naver.com>
2024-08-13 20:15:25 +0900

    feat: 주석 지우기

--- frontend/src/pages/room/CreateRoom.jsx
+++ frontend/src/pages/room/CreateRoom.jsx
@@ -26,8 +26,8 @@ import { savePreset } from '../../apis/preset';
 const CreateRoom = ({ onSave, onClose, item }) => {
   // user정보 불러와야함
   // const user = useSelector((state) => state.user.data.oauth_id);
-  const nickname = ''; //useSelector((state) => state.auth.user.nickname);
-  const userId = ''; //useSelector((state) => state.auth.user.oauthId);
+  const nickname = useSelector((state) => state.auth.user.nickname);
+  const userId = useSelector((state) => state.auth.user.oauthId);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
