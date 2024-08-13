b44d477a60800b5e82dc4bde9305a436479bb25d
강지우 <jiu6525@daum.net>
2024-08-13 12:31:41 +0900

    feat:  프론트 로그인

--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -7,7 +7,8 @@ const PrivateRoute = ({ component: Component }) => {
   const isAuth = useSelector((state) => state.auth.isAuthenticated);
   console.log('----------');
   console.log(isAuth);
-  return isAuth ? Component : <Navigate to='/login' />;
+  // return isAuth ? Component : <Navigate to='/login' />;
+  return isAuth ? Component : <Navigate to='/' />;
 };
 
 export default PrivateRoute;
