d151a498fb36e7dd01d23f2439f02ba4c985510a
kkkhhh4930 <kkkhhh4930@naver.com>
2024-08-13 22:26:11 +0900

    feat: comment 삭제

--- frontend/src/store/actions/feedbackActions.js
+++ frontend/src/store/actions/feedbackActions.js
@@ -7,58 +7,6 @@ import {
 
 const instance = Axios();
 
-// export const fetchFeedback = (oauthId, page) => async (dispatch) => {
-//   dispatch({ type: FEEDBACK_REQUEST });
-
-//   try {
-//     // oauthId 로그 확인
-//     console.log('Using OAuth ID:', oauthId);
-
-//     // 1. 스터디룸 정보를 가져옵니다.
-//     const studyRoomResponse = await instance.post(
-//       '/studyRoom/search',
-//       { oauthId },
-//       {
-//         params: {
-//           page: page,
-//           size: 10,
-//           sort: 'studyRoomId',
-//         },
-//       }
-//     );
-//     console.log('Study Room Response:', studyRoomResponse.data);
-
-//     const studyRooms = studyRoomResponse.data.content;
-
-//     // 2. 스터디룸 정보를 바탕으로 각 스터디룸의 피드백을 가져옵니다.
-//     const feedbackPromises = studyRooms.map((room) =>
-//       instance
-//         .post('/evaluation/feedBack', {
-//           oauthId,
-//           studyRoomId: room.studyRoomId,
-//         })
-//         .then((response) => ({
-//           ...response.data[0],
-//           studyRoomTitle: room.studyRoomTitle,
-//           subject: room.subject,
-//           studyRoomId: room.studyRoomId,
-//         }))
-//     );
-
-//     const responses = await Promise.all(feedbackPromises);
-
-//     // 3. 피드백 데이터를 모두 합쳐서 관리합니다.
-//     const allFeedback = responses.flatMap((response) => response);
-
-//     console.log('All Feedback:', allFeedback);
-
-//     dispatch({ type: FEEDBACK_SUCCESS, payload: allFeedback });
-//   } catch (error) {
-//     dispatch({ type: FEEDBACK_FAILURE, payload: error.message });
-//     console.error('Failed to fetch feedback:', error);
-//   }
-// };
-
 export const fetchFeedbackDetail =
   (oauthId, studyRoomId) => async (dispatch) => {
     dispatch({ type: FEEDBACK_REQUEST });
